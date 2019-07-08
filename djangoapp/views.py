fdbf66f5e456e496c8dd1d5f38e85c945b69e92b
aashishbiradar <aashish.biradar@gmail.com>
2019-07-09 05:03:02 +0530

    fix 'str' object has no attribute 'get'

--- djangoapp/views.py
+++ djangoapp/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from djangoapp.tasks.demo import *
+from django.http import HttpResponse
 
 def send_message(req):
     add.delay(5,4)
-    return '<h1>OK</h1>'
+    return HttpResponse('<h1>OK</h1>')
